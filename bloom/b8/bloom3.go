e1f4975f5833204941411a053cdd0b6dbc36351c
yanagitani <takanori.yanagitani@gmail.com>
2024-10-24 08:55:26 +0900

    docs: more docs

--- bloom/b8/bloom3.go
+++ bloom/b8/bloom3.go
@@ -11,13 +11,16 @@ const (
 	NumberOfHash256bitsDefault int = 6
 )
 
-type Bloom3 [32]uint8 // 256 bits
+// Bloom bits(256 bits).
+type Bloom3 [32]uint8
 
+// Adds the uuid to the bloom bits.
 type UuidToBloom3 func(context.Context, u2b.Uuid, Bloom3) (Bloom3, error)
 
 // Creates 8 "hash functions"(8 bytes) from the [u2b.Uuid].
 type UuidToHash3 func(context.Context, u2b.Uuid) ([8]uint8, error)
 
+// Creates 8 "hash functions" from the "hash"(uuid v4)
 func UuidIvAsHash(_ context.Context, u u2b.Uuid) ([8]uint8, error) {
 	return [8]uint8{
 		u[0x00] ^ u[0x08],
@@ -31,8 +34,10 @@ func UuidIvAsHash(_ context.Context, u u2b.Uuid) ([8]uint8, error) {
 	}, nil
 }
 
+// Adds the hash values([8]uint8) to the bloom bits.
 type AddHashAllToBloom3 func(context.Context, [8]uint8, Bloom3) (Bloom3, error)
 
+// Creates [UuidToBloom3] using [AddHashAllToBloom3].
 func (u UuidToHash3) ToUuidToBloom3(a AddHashAllToBloom3) UuidToBloom3 {
 	return func(ctx context.Context, id u2b.Uuid, b Bloom3) (Bloom3, error) {
 		h, e := u(ctx, id)
@@ -43,7 +48,7 @@ func (u UuidToHash3) ToUuidToBloom3(a AddHashAllToBloom3) UuidToBloom3 {
 	}
 }
 
-// Apply the hash.
+// Adds the hash to the bloom bits.
 //
 //   - 0x00 .. 0x07: modify Bloom3[0x00]
 //   - 0x08 .. 0x0f: modify Bloom3[0x01]
@@ -53,6 +58,7 @@ func (u UuidToHash3) ToUuidToBloom3(a AddHashAllToBloom3) UuidToBloom3 {
 //   - 0xf8 .. 0xff: modify Bloom3[0x1f]
 type AddHashToBloom3 func(context.Context, uint8, Bloom3) (Bloom3, error)
 
+// Creates [AddHashAllToBloom3] using up to [NumberOfHash256bitsDefault] hashs.
 func (a AddHashToBloom3) ToAddHashAll() AddHashAllToBloom3 {
 	return func(ctx context.Context, hash [8]uint8, b Bloom3) (Bloom3, error) {
 		var state Bloom3 = b
@@ -67,8 +73,10 @@ func (a AddHashToBloom3) ToAddHashAll() AddHashAllToBloom3 {
 	}
 }
 
+// Sets the flag to the bloom bits.
 type SetBit3 func(b07 uint8, bloom uint8) uint8
 
+// Creates [AddHashToBloom3].
 func (s SetBit3) ToAddHashToBloom3() AddHashToBloom3 {
 	return func(ctx context.Context, hash uint8, b Bloom3) (Bloom3, error) {
 		var shifted uint8 = hash >> 3
@@ -79,7 +87,7 @@ func (s SetBit3) ToAddHashToBloom3() AddHashToBloom3 {
 	}
 }
 
-// Converts the index of bit to "flag".
+// Converts the index of the bit to the "flag".
 //
 // # Conversion Table
 //
@@ -95,6 +103,7 @@ func (s SetBit3) ToAddHashToBloom3() AddHashToBloom3 {
 //	|   7   | 0x80   |
 type BitIndexToUint8 func(bix uint8) uint8
 
+// Creates [SetBit3].
 func (b BitIndexToUint8) ToSetBit3() SetBit3 {
 	return func(b07 uint8, bloom uint8) uint8 {
 		var shifted uint8 = b(b07)
@@ -102,6 +111,7 @@ func (b BitIndexToUint8) ToSetBit3() SetBit3 {
 	}
 }
 
+// Implements [BitIndexToUint8].
 func BitIxToUint8(bix uint8) uint8 {
 	return 1 << bix
 }
